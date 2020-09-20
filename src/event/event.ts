bd9c9141c5e1be4bd4f8781eccfe3965849193cb
Archie <archie.isdiningrat@gmail.com>
2020-09-20 12:24:41 +0700

    add event

---
+++ src/event/event.ts
@@ -0,0 +1,28 @@
+import Bull from 'bull';
+import { IObject } from '../typings/common';
+
+export interface StaticEventHandler<dispatchPayload = any> {
+    eventName: string;
+    handler(payload: dispatchPayload): Promise<void>
+}
+
+export interface StaticBaseEvent<InstanceBaseEvent, dispatchPayload = IObject> {
+    new(...param: any): InstanceBaseEvent;
+    getInstance(): InstanceBaseEvent;
+    dispatch(payload: dispatchPayload): Promise<void>
+}
+
+export abstract class BaseEvent<PayloadData = IObject> {
+    public queue: Bull.Queue;
+
+    public constructor(eventName: string) {
+        this.queue = new Bull(eventName, String(process.env.QUEUE_WORKER_CONNECTION_STRING));
+        this.queue.process(this.handler);
+    }
+
+    protected abstract handler({ data }: { data: PayloadData }): Promise<void>
+}
+
+export interface StaticEvent {
+    new(...params: any): BaseEvent;
+}
