375d9c143ef1e9a839f5e5f648a5f51ed45b0c4a
Archie <archie.isdiningrat@gmail.com>
2020-09-21 10:37:47 +0700

    export typings to index

--- modules/factory/event_factory.d.ts
+++ modules/factory/event_factory.d.ts
@@ -1,3 +1,3 @@
 import { Event, StaticBaseEvent, StaticEventHandler } from '../event/event';
-export declare const EventFactory: <dispatchPayload = any>(eventHandler: StaticEventHandler<dispatchPayload>) => StaticBaseEvent<Event<import("../typings/common").IObject<any>>, dispatchPayload>;
+export declare const EventFactory: <dispatchPayload = any>(eventHandler: StaticEventHandler<dispatchPayload>) => StaticBaseEvent<Event<import("../typings").IObject<any>>, dispatchPayload>;
 export default EventFactory;
